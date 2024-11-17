8a0c5341ce43da02505aa3cc976d03607aa0f5d7
Andres <andres990420@gmail.com>
2024-11-17 19:40:33 -0400

    Creation of methods Put and Delete in IRepository

--- Orders.Frontend/Repositories/Repository.cs
+++ Orders.Frontend/Repositories/Repository.cs
@@ -1,4 +1,5 @@
-﻿using System.Text;
+﻿using System.Reflection;
+using System.Text;
 using System.Text.Json;
 
 namespace Orders.Frontend.Repositories
@@ -6,10 +7,12 @@ namespace Orders.Frontend.Repositories
     public class Repository : IRepository
     {
         private readonly HttpClient _httpClien;
+
         private readonly JsonSerializerOptions _jsonDefaultOptions = new JsonSerializerOptions()
         {
             PropertyNameCaseInsensitive = true
         };
+
         public Repository(HttpClient httpClien)
         {
             _httpClien = httpClien;
@@ -18,9 +21,9 @@ namespace Orders.Frontend.Repositories
         public async Task<HttpResponseWrapper<T>> GetAsync<T>(string url)
         {
             var responseHttp = await _httpClien.GetAsync(url);
-            if (responseHttp.IsSuccessStatusCode) 
+            if (responseHttp.IsSuccessStatusCode)
             {
-                var response = await UnSerializeAnswer<T>(responseHttp);
+                var response = await UnSerializeAnswerAsync<T>(responseHttp);
                 return new HttpResponseWrapper<T>(response, false, responseHttp);
             }
             return new HttpResponseWrapper<T>(default, false, responseHttp);
@@ -41,13 +44,40 @@ namespace Orders.Frontend.Repositories
             var responseHttp = await _httpClien.PostAsync(url, messangeContent);
             if (responseHttp.IsSuccessStatusCode)
             {
-                var response = await UnSerializeAnswer<TActionResponse>(responseHttp);
+                var response = await UnSerializeAnswerAsync<TActionResponse>(responseHttp);
                 return new HttpResponseWrapper<TActionResponse>(response, false, responseHttp);
             }
             return new HttpResponseWrapper<TActionResponse>(default, true, responseHttp);
         }
 
-        private async Task<T> UnSerializeAnswer<T>(HttpResponseMessage responseHttp)
+        public async Task<HttpResponseWrapper<object>> PutAsync<T>(string url, T model)
+        {
+            var messangeJson = JsonSerializer.Serialize(model);
+            var messangeContent = new StringContent(messangeJson, Encoding.UTF8, "aplication/json");
+            var responseHttp = await _httpClien.PutAsync(url, messangeContent);
+            return new HttpResponseWrapper<object>(null, !responseHttp.IsSuccessStatusCode, responseHttp);
+        }
+
+        public async Task<HttpResponseWrapper<TActionResponse>> PutAsync<T, TActionResponse>(string url, T model)
+        {
+            var messangeJson = JsonSerializer.Serialize(model);
+            var messangeContent = new StringContent(messangeJson, Encoding.UTF8, "aplication/json");
+            var responseHttp = await _httpClien.PutAsync(url, messangeContent);
+            if (responseHttp.IsSuccessStatusCode)
+            {
+                var response = await UnSerializeAnswerAsync<TActionResponse>(responseHttp);
+                return new HttpResponseWrapper<TActionResponse>(response, false, responseHttp);
+            }
+            return new HttpResponseWrapper<TActionResponse>(default, true, responseHttp);
+        }
+
+        public async Task<HttpResponseWrapper<object>> DeleteAsync<T>(string url)
+        {
+            var responseHttp = await _httpClien.DeleteAsync(url);
+            return new HttpResponseWrapper<object>(null, !responseHttp.IsSuccessStatusCode, responseHttp);
+        }
+
+        private async Task<T> UnSerializeAnswerAsync<T>(HttpResponseMessage responseHttp)
         {
             var response = await responseHttp.Content.ReadAsStringAsync();
             return JsonSerializer.Deserialize<T>(response, _jsonDefaultOptions)!;
