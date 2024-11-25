4a95f3dd593d8b32e5fea24a51145f76bb1f72de
Andres <andres990420@gmail.com>
2024-11-24 20:47:39 -0400

    Seeder Creation

---
+++ Orders.Backend/Data/SeedDB.cs
@@ -0,0 +1,52 @@
+﻿
+using Orders.Shared.Entities;
+
+namespace Orders.Backend.Data
+{
+    public class SeedDB
+    {
+        private readonly DataContext _context;
+
+        public SeedDB(DataContext context)
+        {
+            _context = context;
+        }
+
+        public async Task SeedAsync() 
+        {
+            await _context.Database.EnsureCreatedAsync();
+            await CheckCuntriesAsync();
+            await CheckCategoriesAsync();
+        }
+
+        private async Task CheckCategoriesAsync()
+        {
+            if (!_context.Categories.Any())
+            {
+                _context.Categories.Add(new Category { Name = "Belleza" });
+                _context.Categories.Add(new Category { Name = "Apple" });
+                _context.Categories.Add(new Category { Name = "Tecnologia" });
+                _context.Categories.Add(new Category { Name = "Comida" });
+                _context.Categories.Add(new Category { Name = "Juegos" });
+                _context.Categories.Add(new Category { Name = "Accesorios" });
+                _context.Categories.Add(new Category { Name = "Zapatos" });
+                await _context.SaveChangesAsync();
+            }
+        }
+
+        private async Task CheckCuntriesAsync()
+        {
+            if (!_context.Countries.Any()) 
+            {
+                _context.Countries.Add(new Country { Name = "Colombia" });
+                _context.Countries.Add(new Country { Name = "Argentina" });
+                _context.Countries.Add(new Country { Name = "Estados Unidos" });
+                _context.Countries.Add(new Country { Name = "Chile" });
+                _context.Countries.Add(new Country { Name = "Venezuela" });
+                _context.Countries.Add(new Country { Name = "Republica Dominicana" });
+                _context.Countries.Add(new Country { Name = "Paraguay" });
+                await _context.SaveChangesAsync();
+            }
+        }
+    }
+}
--- Orders.Backend/Program.cs
+++ Orders.Backend/Program.cs
@@ -14,10 +14,25 @@ builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 builder.Services.AddDbContext<DataContext>(x => x.UseSqlServer("name=LocalConnection"));
+builder.Services.AddTransient<SeedDB>();
+
 builder.Services.AddScoped(typeof(IGenericUnitOfWork<>), typeof(GenericUnitOfWork<>));
 builder.Services.AddScoped(typeof(IGenericRepository<>), typeof(GenericRepository<>));
 
+
 var app = builder.Build();
+SeedData(app);
+
+void SeedData(WebApplication app) 
+{
+    var scopedFactory = app.Services.GetService<IServiceScopeFactory>();
+
+    using (var scope = scopedFactory!.CreateScope()) 
+    {
+        var service = scope.ServiceProvider.GetService<SeedDB>();
+        service!.SeedAsync().Wait();
+    }
+}
 
 app.UseCors(x => x
     .AllowAnyMethod()
